da459f6d98b18ed85d636922807c2149da9db7ac
Rafał Cisek <rafal.cisek121@gmail.com>
2022-10-19 20:51:41 +0200

    init

---
+++ src/main/java/pl/cisekispolka/App.java
@@ -0,0 +1,13 @@
+package pl.cisekispolka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
